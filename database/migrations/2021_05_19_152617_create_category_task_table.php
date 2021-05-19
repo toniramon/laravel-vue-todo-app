fff6bd8a2413bce0e2aaf2e2c96c24cd7491a3ea
Toni Ramon <toni.ramon93@gmail.com>
2021-05-19 20:44:09 +0200

    :boom: Added seeders

--- database/migrations/2021_05_19_152617_create_category_task_table.php
+++ database/migrations/2021_05_19_152617_create_category_task_table.php
@@ -14,18 +14,17 @@ class CreateCategoryTaskTable extends Migration
     public function up()
     {
         Schema::create('category_task', function (Blueprint $table) {
-            $table->primary(['category_id','task_id']);
-            $table->bigInteger('category_id')->unsigned();
-            $table->bigInteger('task_id')->unsigned();
-            $table->timestamps();
-
             // Foreign Keys
-            $table->foreign('category_id')
-                ->references('id')
-                ->on('categories');
-             $table->foreign('task_id')
-                ->references('id')
-                ->on('tasks');
+            $table->foreignId('category_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('task_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+            $table->primary(['category_id', 'task_id']);
         });
     }
 
